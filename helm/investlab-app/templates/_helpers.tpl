73726a0d182a5bfda8891512ba5fe381e88567de
mkapica <mikolajkapica7@gmail.com>
2025-10-13 14:38:25 +0200

    hide secrets

--- helm/investlab-app/templates/_helpers.tpl
+++ helm/investlab-app/templates/_helpers.tpl
@@ -40,11 +40,17 @@ app.kubernetes.io/instance: {{ .Release.Name }}
 - name: DEBUG
   value: {{ .Values.env.django.debug | quote }}
 - name: SECRET_KEY
-  value: {{ .Values.env.django.secretKey | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: django-secret-key
 - name: ALLOWED_HOSTS
   value: {{ .Values.env.django.allowedHosts | quote }}
 - name: DATABASE_URL
-  value: {{ .Values.env.database.url | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: database-url
 - name: POSTGRES_HOST
   value: {{ .Values.env.database.host | quote }}
 - name: POSTGRES_PORT
@@ -52,33 +58,57 @@ app.kubernetes.io/instance: {{ .Release.Name }}
 - name: POSTGRES_DB
   value: {{ .Values.env.database.name | quote }}
 - name: POSTGRES_USER
-  value: {{ .Values.env.database.user | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: database-user
 - name: POSTGRES_PASSWORD
-  value: {{ .Values.env.database.password | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: database-password
 - name: REDIS_HOST
   value: {{ .Values.env.redis.host | quote }}
 - name: REDIS_PORT
   value: {{ .Values.env.redis.port | quote }}
 - name: REDIS_PASSWORD
-  value: {{ .Values.env.redis.password | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: redis-password
 - name: CORS_ALLOWED_ORIGINS
   value: {{ .Values.env.cors.allowedOrigins | quote }}
 - name: CSRF_TRUSTED_ORIGINS
   value: {{ .Values.env.cors.csrfTrustedOrigins | quote }}
 - name: CLERK_SECRET_KEY
-  value: {{ .Values.env.clerk.secretKey | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: clerk-secret-key
 - name: CLERK_ISSUER
   value: {{ .Values.env.clerk.issuer | quote }}
 - name: CLERK_JWKS_URL
   value: {{ .Values.env.clerk.jwksUrl | quote }}
 - name: CLERK_JWT_KEY
-  value: {{ .Values.env.clerk.jwtKey | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: clerk-jwt-key
 - name: POLYGON_SECRET_KEY
-  value: {{ .Values.env.apis.polygonSecretKey | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: polygon-secret-key
 - name: ALPACA_PUBLIC_KEY
-  value: {{ .Values.env.apis.alpacaPublicKey | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: alpaca-public-key
 - name: ALPACA_SECRET_KEY
-  value: {{ .Values.env.apis.alpacaSecretKey | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: alpaca-secret-key
 - name: ADMIN_EMAIL
   value: {{ .Values.env.email.adminEmail | quote }}
 - name: FROM_EMAIL
@@ -88,7 +118,10 @@ app.kubernetes.io/instance: {{ .Release.Name }}
 - name: EMAIL_FILE_PATH
   value: {{ .Values.env.email.filePath | quote }}
 - name: VAPID_PRIVATE_KEY
-  value: {{ .Values.env.vapid.privateKey | quote }}
+  valueFrom:
+    secretKeyRef:
+      name: investlab-app-secrets
+      key: vapid-private-key
 - name: VAPID_PUBLIC_KEY
   value: {{ .Values.env.vapid.publicKey | quote }}
 {{- end }}
--- modules/application/main.tf
+++ modules/application/main.tf
@@ -18,6 +18,42 @@ locals {
   }
 }
 
+# Create Kubernetes secret for sensitive values
+resource "kubernetes_secret" "app_secrets" {
+  metadata {
+    name      = "investlab-app-secrets"
+    namespace = var.kubernetes_namespace
+    labels    = local.common_labels
+  }
+
+  type = "Opaque"
+
+  data = {
+    # Database credentials
+    database-password = var.config.database.password
+    database-user     = var.config.database.user
+    database-url      = "postgres://${var.config.database.user}:${var.config.database.password}@${var.config.database.host}:5432/${var.config.database.name}"
+
+    # Redis credentials
+    redis-password = var.config.redis.password
+
+    # Django settings
+    django-secret-key = var.config.django.secret_key
+
+    # Clerk authentication
+    clerk-secret-key = var.config.auth.clerk_secret_key
+    clerk-jwt-key    = var.config.auth.clerk_jwt_key
+
+    # API keys
+    polygon-secret-key = var.config.apis.polygon_secret_key
+    alpaca-secret-key  = var.config.apis.alpaca_secret_key
+    alpaca-public-key  = var.config.apis.alpaca_public_key
+
+    # VAPID keys
+    vapid-private-key = var.config.vapid.private_key
+  }
+}
+
 resource "helm_release" "investlab_app" {
   name      = "investlab-app"
   namespace = var.kubernetes_namespace
@@ -33,23 +69,22 @@ resource "helm_release" "investlab_app" {
 
       env = {
         database = {
-          host     = var.config.database.host
-          port     = 5432
-          name     = var.config.database.name
-          user     = var.config.database.user
-          password = var.config.database.password
+          host = var.config.database.host
+          port = 5432
+          name = var.config.database.name
+          # Remove sensitive values - they will be loaded from secrets
         }
 
         redis = {
-          host     = var.config.redis.host
-          port     = 6379
-          password = var.config.redis.password
+          host = var.config.redis.host
+          port = 6379
+          # Remove sensitive values - they will be loaded from secrets
         }
 
         django = {
           logLevel     = var.config.django.log_level
           debug        = var.config.django.debug
-          secretKey    = var.config.django.secret_key
+          # Remove secretKey - it will be loaded from secrets
           allowedHosts = var.config.django.allowed_hosts
         }
 
@@ -59,16 +94,13 @@ resource "helm_release" "investlab_app" {
         }
 
         clerk = {
-          secretKey = var.config.auth.clerk_secret_key
-          issuer    = var.config.auth.clerk_issuer
-          jwksUrl   = var.config.auth.clerk_jwks_url
-          jwtKey    = var.config.auth.clerk_jwt_key
+          # Remove sensitive values - they will be loaded from secrets
+          issuer  = var.config.auth.clerk_issuer
+          jwksUrl = var.config.auth.clerk_jwks_url
         }
 
         apis = {
-          polygonSecretKey = var.config.apis.polygon_secret_key
-          alpacaPublicKey  = var.config.apis.alpaca_public_key
-          alpacaSecretKey  = var.config.apis.alpaca_secret_key
+          # Remove sensitive values - they will be loaded from secrets
         }
 
         email = {
@@ -79,8 +111,8 @@ resource "helm_release" "investlab_app" {
         }
 
         vapid = {
-          privateKey = var.config.vapid.private_key
-          publicKey  = var.config.vapid.public_key
+          # Remove sensitive values - they will be loaded from secrets
+          publicKey = var.config.vapid.public_key
         }
       }
 
@@ -92,6 +124,7 @@ resource "helm_release" "investlab_app" {
 
   depends_on = [
     var.kubernetes_namespace,
-    var.letsencrypt_issuer
+    var.letsencrypt_issuer,
+    kubernetes_secret.app_secrets
   ]
 }
