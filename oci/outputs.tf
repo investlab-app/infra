7416852f4b77c3a17e595e211330a30e413f084e
mkapica <mikolajkapica7@gmail.com>
2025-11-28 22:22:04 +0100

    add outputs

--- oci/outputs.tf
+++ oci/outputs.tf
@@ -14,6 +14,14 @@ output "node_pool_id" {
   value = oci_containerengine_node_pool.k8s_node_pool.id
 }
 
+output "vault_id" {
+  value = oci_kms_vault.k8s_vault.id
+}
+
+output "key_id" {
+  value = oci_kms_key.k8s_master_key.id
+}
+
 output "kubernetes_version" {
   value = var.kubernetes_version
 }
